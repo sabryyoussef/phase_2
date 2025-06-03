93f34c88207d4e5282e3786f3b370215e3a4b46a
sabry_youssef <vendorah2@gmail.com>
2025-06-04 02:26:54 +0300

    adding the missed view

--- account_invoice_report/__manifest__.py
+++ account_invoice_report/__manifest__.py
@@ -5,7 +5,8 @@
     'data': [
         'views/views.xml',
         'views/invoices.xml',
-        # 'views/sales.xml',
+        'views/statement_report.xml',
+        'views/sales.xml',
         'views/receipt_voucher.xml',
         'views/receipt_voucher_invoices.xml',
     ],
