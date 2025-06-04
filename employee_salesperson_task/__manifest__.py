dbee4dc25c97a176e0a832519d99d5e8512e27fa
sabry_youssef <vendorah2@gmail.com>
2025-06-04 04:55:24 +0300

    emp sales person task

---
+++ employee_salesperson_task/__manifest__.py
@@ -0,0 +1,9 @@
+{
+    'name': "Employee Salesperson Task",
+    'author': "BeshoyWageh",
+    # 'version': '16.0',
+    'depends': ['base','hr'],
+    'data': [
+        'views/employee.xml',
+    ],
+}
