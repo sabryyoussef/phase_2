14d163495975ce5beba1ab4e140affa80c17e440
sabry_youssef <vendorah2@gmail.com>
2025-06-04 05:01:24 +0300

    partner custom fields

---
+++ partner_custom_fields/__manifest__.py
@@ -0,0 +1,9 @@
+{
+    'name': "Partner Custom Fields",
+    'author': "Beshoy Wageh",
+    'version': '0.1',
+    'depends': ['base','hr','partner_risk_assessment'],
+    'data': [
+        'views/partner.xml',
+    ],
+}
---
+++ partner_custom_fields/models/partner.py
@@ -0,0 +1,10 @@
+
+from odoo import api, fields, models
+
+class Partner(models.Model):
+    _inherit = 'res.partner'
+
+    primary_support_id = fields.Many2one('hr.employee', string='Primary Support')
+    secondary_support_id = fields.Many2one('hr.employee', string='Secondary Support')
+    accountant1_id = fields.Many2one('hr.employee', string='Accountant 1')
+    accountant2_id = fields.Many2one('hr.employee', string='Accountant 2')
