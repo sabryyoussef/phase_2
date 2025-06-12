68fb4941baee9eedf1c427769712e7f8e010986f
sabry_youssef <vendorah2@gmail.com>
2025-06-12 16:29:09 +0300

    product restriction

---
+++ product_restriction/__manifest__.py
@@ -0,0 +1,13 @@
+{
+    'name': 'Product Restriction',
+    # 'version': '16.0.0.0',
+    "author": "Beshoy Wageh",
+    'depends': ['base','sale_management'],
+    'data': [
+            'security/security.xml',
+            'views/views.xml',
+    ],
+    'license': 'OPL-1',
+    "auto_install": False,
+    "installable": True,
+}
---
+++ product_restriction/models/users.py
@@ -0,0 +1,21 @@
+
+from odoo import api, fields, models, _
+
+class ResUsers(models.Model):
+    _inherit = 'res.users'
+
+    restriction_on = fields.Selection([('product', 'Product'),
+                                ('category', 'Category'),
+                                ], string='Restriction On')
+
+    product_ids = fields.Many2many('product.product', string="Products")
+    categories_ids=fields.Many2many('product.category',string="category")
+
+
+    @api.onchange('restriction_on')
+    def _onchange_restriction_on(self):
+        if self.restriction_on == 'product':
+            self.product_ids=[(6,0,[])]
+        elif self.restriction_on == 'category':
+            self.categories_ids = [(6,0,[])]
+
