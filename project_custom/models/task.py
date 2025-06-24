ef59cbb648334f653f5bfdc43195a8562d0744ae
Sabry Youssef <vendorah2@gmail.com>
2025-06-24 20:02:35 +0300

    fix: Correct _read_group_stage_ids in project_custom module\n\n- Remove 'order' parameter from _read_group_stage_ids method\n- Use stages._order instead of order parameter\n- Fixes conflicting method signatures between modules

--- project_custom/models/task.py
+++ project_custom/models/task.py
@@ -41,12 +41,12 @@ class Tasks(models.Model):
                     task.sudo().milestone_id.task_ids = [task.id]
 
     @api.model
-    def _read_group_stage_ids(self, stages, domain, order):
+    def _read_group_stage_ids(self, stages, domain):
         search_domain = [("id", "in", stages.ids)]
         if "default_project_id" in self.env.context:
             search_domain = [
                 "|",
                 ("project_ids", "=", self.env.context["default_project_id"]),
             ] + search_domain
-        stage_ids = stages._search(search_domain, order=order)
+        stage_ids = stages._search(search_domain, order=stages._order)
         return stages.browse(stage_ids)
