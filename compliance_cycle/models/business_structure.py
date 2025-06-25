a0db3246c60d66cebafe3953bd7e627c49d29cd7
Sabry Youssef <vendorah2@gmail.com>
2025-06-25 19:15:27 +0300

    Add missing fields to res.partner.business.shareholder model for project view compatibility

--- compliance_cycle/models/business_structure.py
+++ compliance_cycle/models/business_structure.py
@@ -26,8 +26,69 @@ class BusinessShareholder(models.Model):
     _name = "res.partner.business.shareholder"
     _description = "Business Shareholder"
 
-    partner_id = fields.Many2one("res.partner")
+    # Project and partner relations
+    project_id = fields.Many2one("project.project", string="Project")
+    customer_id = fields.Many2one("res.partner", string="Customer")
+    partner_id = fields.Many2one("res.partner", string="Partner")
     contact_id = fields.Many2one("res.partner", string="Contact")
     ubo_id = fields.Many2one("res.partner.ubo", string="UBO")
+
+    # Shareholding information
     shareholding = fields.Float(string="Shareholding")
     relationship_ids = fields.Many2many("business.relationships", string="Relationship")
+
+    # Contact information
+    email = fields.Char(string="Email")
+    mobile = fields.Char(string="Mobile")
+    company_type = fields.Selection(
+        [("person", "Individual"), ("company", "Company")],
+        string="Company Type",
+        default="person",
+    )
+
+    # Individual information
+    nationality_id = fields.Many2one("res.nationality", string="Nationality")
+    gender = fields.Selection([("male", "Male"), ("female", "Female")], string="Gender")
+
+    # Corporate information
+    license_authority_id = fields.Many2one(
+        "product.attribute.value", string="License Authority"
+    )
+    incorporation_date = fields.Date(string="Incorporation Date")
+    license_number = fields.Char(string="License Number")
+    license_validity = fields.Selection(
+        [
+            ("1", "1 Year"),
+            ("2", "2 Years"),
+            ("3", "3 Years"),
+            ("4", "4 Years"),
+            ("5", "5 Years"),
+            ("6", "6 Years"),
+            ("7", "7 Years"),
+            ("8", "8 Years"),
+            ("9", "9 Years"),
+            ("10", "10 Years"),
+        ],
+        string="License Validity",
+    )
+
+    # Document references
+    documents_folder_id = fields.Many2one("documents.folder", string="Documents Folder")
+    passport = fields.Many2one("client.document", string="Passport")
+    uae_resident = fields.Boolean(string="UAE Resident")
+    eid_copy = fields.Many2one("client.document", string="EID Copy")
+    residence_visa_copy = fields.Many2one(
+        "client.document", string="Residence Visa Copy"
+    )
+    current_visa = fields.Many2one("client.document", string="Current Visa")
+    entry_stamp = fields.Many2one("client.document", string="Entry Stamp")
+    trade_license = fields.Many2one("client.document", string="Trade License")
+    memorandum_association = fields.Many2one(
+        "client.document", string="Memorandum of Association"
+    )
+    apply_visa = fields.Boolean(string="Apply Visa")
+
+    # Address relation
+    address_ids = fields.One2many(
+        "res.partner.address", "shareholder_id", string="Addresses"
+    )
