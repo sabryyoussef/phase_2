005630f0e8e78da67abaed713281d38c32aacd73
Sabry Youssef <vendorah2@gmail.com>
2025-06-25 19:40:44 +0300

    Fix model references: Change res.nationality to res.country and client.document to res.partner.document

--- compliance_cycle/models/business_structure.py
+++ compliance_cycle/models/business_structure.py
@@ -47,7 +47,7 @@ class BusinessShareholder(models.Model):
     )
 
     # Individual information
-    nationality_id = fields.Many2one("res.nationality", string="Nationality")
+    nationality_id = fields.Many2one("res.country", string="Nationality")
     gender = fields.Selection([("male", "Male"), ("female", "Female")], string="Gender")
 
     # Corporate information
@@ -74,16 +74,16 @@ class BusinessShareholder(models.Model):
 
     # Document references
     documents_folder_id = fields.Many2one("documents.folder", string="Documents Folder")
-    passport = fields.Many2one("client.document", string="Passport")
+    passport = fields.Many2one("res.partner.document", string="Passport")
     uae_resident = fields.Boolean(string="UAE Resident")
-    eid_copy = fields.Many2one("client.document", string="EID Copy")
+    eid_copy = fields.Many2one("res.partner.document", string="EID Copy")
     residence_visa_copy = fields.Many2one(
-        "client.document", string="Residence Visa Copy"
+        "res.partner.document", string="Residence Visa Copy"
     )
-    current_visa = fields.Many2one("client.document", string="Current Visa")
-    entry_stamp = fields.Many2one("client.document", string="Entry Stamp")
-    trade_license = fields.Many2one("client.document", string="Trade License")
+    current_visa = fields.Many2one("res.partner.document", string="Current Visa")
+    entry_stamp = fields.Many2one("res.partner.document", string="Entry Stamp")
+    trade_license = fields.Many2one("res.partner.document", string="Trade License")
     memorandum_association = fields.Many2one(
-        "client.document", string="Memorandum of Association"
+        "res.partner.document", string="Memorandum of Association"
     )
     apply_visa = fields.Boolean(string="Apply Visa")
